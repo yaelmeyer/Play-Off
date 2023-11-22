cd9475b7fb91620dddd517639b1394f554802b71
yaelmeyer <yaelyaelmeyer@gmail.com>
2023-11-22 16:51:39 -0300

    duelo avanza de pj a pj

--- front/src/pages/juego/Duelo.js
+++ front/src/pages/juego/Duelo.js
@@ -6,15 +6,21 @@ import { useEffect, useState } from "react"
 const ROOT = '/img/carrucel'
 
 const Duelo = (props) =>{
+    const pos1Actual = props.pos1Actual
+    const pos2Actual = props.pos2Actual
+    const setPos1Actual = props.setPos1Actual
+    const setPos2Actual = props.setPos2Actual
+
     let pj1 = require(`../../${props.pjs[0]}`) 
     let pj2 = require(`../../${props.pjs[1]}`) 
-    let ganador = require(`../../${props.pjs[props.ganador]}`) 
     const vs = require('../../img/vs.png')
-    const setSiguiente = props.setSiguiente
+    const setEliminatoria = props.setEliminatoria
 
 
     const votar =(index) =>{
-        setSiguiente(index)
+        console.log(index)
+        setPos1Actual(pos1Actual+2)
+        setPos2Actual(pos2Actual+2)
     }
 
     return(
@@ -23,7 +29,6 @@ const Duelo = (props) =>{
             <img src={vs} key={vs}/>
             <img src={pj2} key={pj2} onClick={() =>votar(1)}/>
             <br/>
-            <img src={ganador} key={ganador}/>
         </div>
     )
 }
--- front/src/pages/juego/Eliminatoria.js
+++ front/src/pages/juego/Eliminatoria.js
@@ -2,18 +2,34 @@ import { useState } from 'react'
 import Duelo from './Duelo'
 
 const Eliminatoria = (props) =>{
-    const url1 = 'img/carrucel/pj6.png'
+    const cantPjs = 4
+    const url1 = 'img/carrucel/pj1.png'
     const url2 = 'img/carrucel/pj2.png'
-    const pjs = [url1, url2] 
-    const [siguiente, setSiguiente] = useState(0)
+    const url3 = 'img/carrucel/pj3.png'
+    const url4 = 'img/carrucel/pj4.png'
+    const url5 = 'img/carrucel/pj5.png'
+    const url6 = 'img/carrucel/pj6.png'
+    const url7 = 'img/carrucel/pj7.png'
+    const url8 = 'img/carrucel/pj8.png'
+
+    let urls = [url1, url2, url3, url4, url5, url6, url7, url8]
+
+    const [pos1Actual, setPos1Actual] = useState(0)
+    const [pos2Actual, setPos2Actual] = useState(1)
+
+    let url1Actual = urls[pos1Actual]
+    let url2Actual = urls[pos2Actual]
+    let pjs = [url1Actual, url2Actual] 
+    // const [eliminatoria, setEliminatoria] = useState(null)
 
     return(
         <>
             <Duelo  pjs= {pjs}
-                    setSiguiente={setSiguiente}
-                    ganador={siguiente}>
+                    pos1Actual={pos1Actual}
+                    pos2Actual={pos2Actual}
+                    setPos1Actual={setPos1Actual}
+                    setPos2Actual={setPos2Actual}>
             </Duelo>
-            <span>{siguiente}</span>
         </>
     )
 }
