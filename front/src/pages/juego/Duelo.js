728ca4be4a3c11a7906e2b74fd7f291c0ff9c213
yaelmeyer <yaelyaelmeyer@gmail.com>
2023-11-23 10:43:42 -0300

    flujo de eliminatoria hasta la final

--- front/src/pages/juego/Duelo.js
+++ front/src/pages/juego/Duelo.js
@@ -6,21 +6,55 @@ import { useEffect, useState } from "react"
 const ROOT = '/img/carrucel'
 
 const Duelo = (props) =>{
+//props _____________________________________________________
     const pos1Actual = props.pos1Actual
     const pos2Actual = props.pos2Actual
     const setPos1Actual = props.setPos1Actual
     const setPos2Actual = props.setPos2Actual
 
+    const setUrlsActual = props.setUrlsActual
+    const setUrlsSiguiente = props.setUrlsSiguiente
+
+    let fase = props.fase
+    const setFase = props.setFase
+
+    const setGanador = props.setGanador
+//_____________________________________________________________
+
+//variables___________________________________________________
     let pj1 = require(`../../${props.pjs[0]}`) 
     let pj2 = require(`../../${props.pjs[1]}`) 
-    const vs = require('../../img/vs.png')
-    const setEliminatoria = props.setEliminatoria
+    const vs = require('../../img/vs.png') 
+//______________________________________________________________
 
+//variables de logica___________________________________________
+    const [cont, setCont] = useState(2);
+    const [pjsGanadores, setPjsGanadores] = useState([])
+//________________________________________________________________
+
+    console.log('fase: '+ fase)
 
     const votar =(index) =>{
-        console.log(index)
+        // debugger
+       if(pjsGanadores.length < fase/2){
+        pjsGanadores.push(props.pjs[index])
+        setPjsGanadores(pjsGanadores)
         setPos1Actual(pos1Actual+2)
         setPos2Actual(pos2Actual+2)
+        setCont(cont + 2) 
+       }
+       if(pjsGanadores.length!=1 && pjsGanadores.length == fase/2){
+        setUrlsActual(pjsGanadores)
+        setCont(2)
+        setFase(fase/2)
+        setPos1Actual(0)
+        setPos2Actual(1)
+        setPjsGanadores([])
+       } 
+
+       if(pjsGanadores.length == 1 && fase == 1){
+        setGanador(pjsGanadores[0])
+       }
     }
 
     return(
--- front/src/pages/juego/Eliminatoria.js
+++ front/src/pages/juego/Eliminatoria.js
@@ -12,24 +12,43 @@ const Eliminatoria = (props) =>{
     const url7 = 'img/carrucel/pj7.png'
     const url8 = 'img/carrucel/pj8.png'
 
-    let urls = [url1, url2, url3, url4, url5, url6, url7, url8]
+    const [urlsActual, setUrlsActual] = useState([url1, url2, url3, url4, url5, url6, url7, url8])
+
+    const [urlsSiguiente, setUrlsSiguiente] = useState([])
 
     const [pos1Actual, setPos1Actual] = useState(0)
     const [pos2Actual, setPos2Actual] = useState(1)
 
-    let url1Actual = urls[pos1Actual]
-    let url2Actual = urls[pos2Actual]
+    let url1Actual = urlsActual[pos1Actual]
+    let url2Actual = urlsActual[pos2Actual]
     let pjs = [url1Actual, url2Actual] 
-    // const [eliminatoria, setEliminatoria] = useState(null)
+    
+    const [fase, setFase] = useState(urlsActual.length)
+
+    const [ganador, setGanador] = useState(null)
 
     return(
         <>
-            <Duelo  pjs= {pjs}
+            {!ganador?(
+                <Duelo  pjs= {pjs}
                     pos1Actual={pos1Actual}
                     pos2Actual={pos2Actual}
                     setPos1Actual={setPos1Actual}
-                    setPos2Actual={setPos2Actual}>
+                    setPos2Actual={setPos2Actual}
+                    fase = {fase}
+                    setFase= {setFase}
+                    setUrlsActual={setUrlsActual}
+                    setUrlsSiguiente={setUrlsSiguiente}
+                    setGanador={setGanador}>
             </Duelo>
+            ) :
+            <div>
+                <h2>Ganador:</h2>
+                <img src={ganador} />
+            </div>
+            }
+            
+            
         </>
     )
 }
@@ -41,3 +60,8 @@ export default Eliminatoria
 //     <span>true</span>
 // ) :
 // (<span>false</span>)}
+
+
+// {urlsActual.map((url) => (
+//     <p key={url}>{url}</p>
+// ))}
