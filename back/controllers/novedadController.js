bef0360c0c88ca8a33eec04caab51e2a31a5daa6
yaelmeyer <yaelyaelmeyer@gmail.com>
2023-11-28 10:20:08 -0300

    crud de novedades

--- back/controllers/novedadController.js
+++ back/controllers/novedadController.js
@@ -3,29 +3,66 @@ let pool = require('../db/conectorDB')
 
 const novedadGet = async(req = request, res = response) =>{
 
+    const query = 'select * from novedad'
+
+    const result = await pool.query(query)
+
     res.json({
-        msg: 'se obtiene novedad'
+        msg: 'se obtubieron las novedades',
+        novedades: result
     })
+}
+
+const novedadGetById = async(id) =>{
+    const query = 'select * from novedad where id = ?'
+
+    const novedad = await pool.query(query, [id])
+
+    return novedad[0]
 }
 
 const novedadPost = async(req = request, res = response) =>{
+    const {titulo, descripcion} = req.body
+    const query = `insert into novedad (titulo, descripcion) values (?, ?) `
+    console.log("holaa: "+titulo+" "+descripcion)
+    await pool.query(query, [titulo, descripcion])
 
-    res.json({
-        msg: 'se guarda novedad'
-    })
+    res.redirect('/novedades/admin')
 }
 
 const novedadDelete = async(req=request, res=response) =>{
+    const id = req.params.id
+    const query = 'delete from novedad where novedad.id = ?'
+    
+    await pool.query(query, [id])
 
-    res.json({
-        msg: 'se elimina novedad'
-    })
+    res.redirect('/novedades/admin')
 }
 
 const novedadUpdate = async(req = request, res = response) =>{
+    const {id, titulo, descripcion} = req.body
+    const query = 'update novedad set titulo = ?, descripcion = ? where id = ?'
 
-    res.json({
-        msg: 'se actualiza novedad'
+    await pool.query(query, [titulo, descripcion, id])
+    res.redirect('/novedades/admin')
+}
+
+const inicio = async(req = request, res=response) =>{
+    const query = 'select * from novedad'
+    const result = await pool.query(query)
+    console.log(result)
+    res.render('admin/novedades',{
+        novedades: result,
+        holi: 'sooo'
+    })
+}
+
+const actualizar = async(req = request, res=response) =>{
+    const id = req.params.id
+    const novedad = await novedadGetById(id)
+
+    res.render('admin/novedades/modificar',{
+        novedad: novedad
     })
 }
 
@@ -33,5 +70,7 @@ module.exports = {
     novedadGet,
     novedadPost,
     novedadDelete,
-    novedadUpdate
+    novedadUpdate,
+    inicio,
+    actualizar
 }
--- back/routes/novedadRoute.js
+++ back/routes/novedadRoute.js
@@ -1,14 +1,18 @@
 const {Router} = require('express')
-const {novedadGet, novedadPost, novedadDelete, novedadUpdate} = require('../controllers/novedadController')
+const {novedadGet, novedadPost, novedadDelete, novedadUpdate, inicio, actualizar} = require('../controllers/novedadController')
 
 const router = Router()
+
+router.get('/admin', inicio)
 
 router.get('/', novedadGet)
 
 router.post('/guardar', novedadPost)
 
-router.delete('/eliminar', novedadDelete)
+router.get('/eliminar/:id', novedadDelete)
 
-router.put('/actualizar', novedadUpdate)
+router.get('/form-actualizar/:id', actualizar)
+
+router.post('/actualizar', novedadUpdate)
 
 module.exports = router
